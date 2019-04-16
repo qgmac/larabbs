c8d549fca0bc46beffca0e74586151266b01c7cd
vagrant <vagrant@homestead>
2019-04-16 09:11:55 +0000

    话题排序

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -8,8 +8,8 @@
     <div class="card">
         <div class="card-header gb-transparent">
           <ul class="nav nav-pills">
-            <li class="nav-item"><a class="nav-link active">最后回复</a></li>
-            <li class="nav-item"><a class="nav-link">最新发布</a></li>
+            <li class="nav-item"><a class="nav-link {{ active_class( ! if_query('order', 'recent')) }}" href="{{ Request::url() }}?order=default">最后回复</a></li>
+            <li class="nav-item"><a class="nav-link {{ active_class(if_query('order', 'recent')) }}" href="{{ Request::url() }}?order=recent">最新发布</a></li>
           </ul>
         </div>
         <div class="card-body">
