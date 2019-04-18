bd2803d82af09a21d861965e17c25aefb392f186
vagrant <vagrant@homestead>
2019-04-18 11:13:36 +0000

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,5 @@ Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+Route::get('permission-denied', 'PageController@permissionDenied')->name('permission-denied');
