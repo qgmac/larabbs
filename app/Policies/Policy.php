ad39e577948b40f33f40fc8a07ed8adbebcee83b
vagrant <vagrant@homestead>
2019-04-18 09:03:12 +0000

    站点权限部署

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -15,6 +15,12 @@ class Policy
 
     public function before($user, $ability)
 	{
+	    // 如果用户拥有管理内容的权限的haul，即授权通过
+
+        if ($user->can('manage_contents')) {
+            return true;
+        }
+
 	    // if ($user->isSuperAdmin()) {
 	    // 		return true;
 	    // }
