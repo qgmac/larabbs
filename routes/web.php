8b358d2768179a1fad88f4443381766fcdb04e21
vagrant <vagrant@homestead>
2019-04-19 08:51:10 +0000

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', 'PageController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 //Auth::routes();
 
